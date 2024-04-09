3ee30808129d1499976f48deefde1955b9f2e0df
marco-garro <mgarro@elemento.cloud>
2024-04-09 17:48:44 +0200

    call to weather api

--- lib/Weather_api.dart
+++ lib/Weather_api.dart
@@ -1,23 +1,53 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 class Weather_api{
 
-  String _api_url = "https://api.open-meteo.com/v1/forecast?latitude=0&longitude=0";
-  String latitude = "";
-  String longitude = "";
+  final String _api_url = "api.open-meteo.com";
+  String _latitude = "44.59703140";
+  String _longitude = "7.61142170";
   Weather_api();
+
 
   String get api_url => _api_url;
 
-  void api_parameter_set(String latitude, String longitude) {
-    _api_url += "latitude=$latitude&longitude=$longitude";
-  }
-
-  factory Weather_api.call_weather_api(String latitude, String longitude){
+  factory Weather_api.from_geo(String latitude, String longitude){
     Weather_api w = Weather_api();
-    w.api_parameter_set(latitude, longitude);
+    w._latitude = latitude;
+    w._longitude = longitude;
     return w;
   }
 
   String toString(){
-    return "url: $api_url, latitude: $latitude, longitude: $longitude";
+    return "url: $api_url, latitude: $_latitude, longitude: $_longitude";
+  }
+
+  Map<String, dynamic>? call_api(){
+    const path = "/v1/forecast";
+    Map<String, dynamic> params = {"longitude":_longitude,"latitude": _latitude,
+      "current": "temperature_2m,wind_speed_10m",
+      "hourly":"temperature_2m,relative_humidity_2m,wind_speed_10m"};
+    Uri uri = Uri.https(api_url,path, params);
+    print(uri.query);
+    http.get(uri).then(
+            (result){
+
+              print(json.decode(result.body));
+        }
+    );
+    return null;
+  }
+
+  String get latitude => _latitude;
+
+  set latitude(String value) {
+    _latitude = value;
+  }
+
+  String get longitude => _longitude;
+
+  set longitude(String value) {
+    _longitude = value;
   }
 }
