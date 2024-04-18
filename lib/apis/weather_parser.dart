26630eeedf4465531b1199be0a6eaa24a717d1aa
marco-garro <marcogarro313@gmail.com>
2024-04-18 08:17:49 +0200

    Current attributes types updated

--- lib/apis/weather_parser.dart
+++ lib/apis/weather_parser.dart
@@ -7,20 +7,20 @@ class Current {
   final String windDirection;
   final int weatherCode;
   final int precipitation;
-  final double humidity;
+  final int humidity;
   Current(this.temperature, this.apparentTemperature, this.windSpeed,
       this.windDirection, this.weatherCode, this.precipitation, this.humidity);
 
   factory Current.fromJson(Map<String, dynamic> json){
 
     Current c = Current(
-        json["current"]["temperature_2m"],
-        json["current"]["relative_humidity_2m"],
-        json["current"]["apparent_temperature"],
-        json["current"]["precipitation"],
-        json["current"]["weather_code"],
-        json["current"]["wind_speed_10m"],
-        json["current"]["wind_direction_10m"],
+      json["current"]["temperature_2m"],
+      json["current"]["apparent_temperature"],
+      json["current"]["wind_speed_10m"],
+      "$json['current']['wind_direction_10m']",
+      json["current"]["weather_code"],
+      json["current"]["precipitation"],
+      json["current"]["relative_humidity_2m"],
     );
 
     return c;
