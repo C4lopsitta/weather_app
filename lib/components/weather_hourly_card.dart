9bcd7a45d698f13e04fd311002e33fb75d570495
c4lopsitta <simone@atomtech.cc>
2024-04-21 17:14:38 +0200

    Added hourly weather card

--- lib/components/weather_hourly_card.dart
+++ lib/components/weather_hourly_card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:weather_app/components/icon_text.dart';
 import '../forecast/Hourly.dart';
 
 class HourlyWeatherCard extends StatefulWidget {
@@ -14,7 +14,7 @@ class HourlyWeatherCard extends StatefulWidget {
   State<StatefulWidget> createState() => _HourlyWeatherCard();
 }
 
-class _HourlyWeatherCard extends State<StatefulWidget> {
+class _HourlyWeatherCard extends State<HourlyWeatherCard> {
   @override
   void initState() {
     super.initState();
@@ -22,23 +22,23 @@ class _HourlyWeatherCard extends State<StatefulWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      child: Card( child: Padding(
         padding: EdgeInsets.all(12),
         child: Column(
           children: [
-            Text("Forecast for the next 24 hours"),
+            const Text("Forecast for the next 24 hours", style: TextStyle(fontSize: 16, height: 1.75)),
+            const Divider(),
             SingleChildScrollView(
               scrollDirection: Axis.horizontal,
               child: Row(
-                children: [
-
-                ],
+                children: _HourlyTile.buildList(_HourlyTile.tilesFromAPI(widget.hourly), context)
               ),
             )
           ],
         ),
-      ),
+      )),
     );
   }
 }
@@ -58,13 +58,38 @@ class _HourlyTile {
   double precipitationProbability;
   double humidity;
 
-  Widget build() {
-    return Text("Banana");
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      width: MediaQuery.of(context).size.width * 0.15,
+      height: MediaQuery.of(context).size.height * 0.20,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(date),
+          Icon(icon, size: 30),
+          Text("${temp.round()}°"),
+          const SizedBox(height: 5),
+          Text("${humidity.round()}%", style: const TextStyle(fontSize: 12)),
+          IconedText(
+            icon: Icons.water_drop,
+            text: "${precipitationProbability.round()}%",
+            iconSize: 16,
+            textStyle: const TextStyle(fontSize: 12),
+          )
+        ],
+      ),
+    );
   }
   
-  static List<Widget> buildList(List<_HourlyTile> tiles) {
+  static List<Widget> buildList(List<_HourlyTile> tiles, BuildContext  context) {
     List<Widget> list = [];
-    tiles.forEach((tile) => list.add(tile.build()));
+    tiles.forEach((tile) {
+      list.add(tile.build(context));
+      list.add(const SizedBox(width: 10));
+    });
     return list;
   }
 
