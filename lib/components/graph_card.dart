cb3459682a73aadb3f40724f5e6838627d628b74
c4lopsitta <simone@atomtech.cc>
2024-04-23 21:29:34 +0200

    Added code for searchbar in historical weather

--- lib/components/graph_card.dart
+++ lib/components/graph_card.dart
@@ -1,5 +1,6 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class GraphCard extends StatefulWidget {
   GraphCard({
@@ -20,13 +21,72 @@ class GraphCard extends StatefulWidget {
 }
 
 class _GraphCard extends State<GraphCard> {
+  LineChartData buildData() {
+    var (range, type) = _GraphUtilities.getRangeSize(widget.graphStart, widget.graphEnd);
+
+    return LineChartData(
+      minX: 0,
+      minY: 0,
+      maxX: range * 1.0,
+      maxY: 10,
+
+      gridData: FlGridData(
+        show: true,
+        drawVerticalLine: true,
+        horizontalInterval: 1,
+        verticalInterval: _GraphUtilities.getXGap(widget.graphStart, widget.graphEnd) * 1.0,
+        getDrawingHorizontalLine: (value) {
+          return const FlLine(
+            color: Colors.blueGrey,
+            strokeWidth: 1
+          );
+        },
+        getDrawingVerticalLine: (value) {
+          return const FlLine(
+            color: Colors.blueGrey,
+            strokeWidth: 1
+          );
+        }
+      ),
+
+      titlesData: FlTitlesData(
+        show: true,
+        rightTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        topTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            interval: _GraphUtilities.getXGap(widget.graphStart, widget.graphEnd) * 1.0,
+            reservedSize: (type < 2) ? 25 : 35,
+            getTitlesWidget: (value, meta) {
+              return RotatedBox(
+                quarterTurns: -3,
+                child: Text(_GraphUtilities.getHorizontalLabel(widget.graphStart, widget.graphEnd, type, value), style: graphLabel),
+              );
+            }
+          )
+        ),
+        leftTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            interval: 10,
+            reservedSize: 30,
+            getTitlesWidget: (value, meta) {
+              return Text('abc', style: graphLabel);
+            }
+          )
+        )
+      )
+    );
+  }
+
   LineChartData mainData() {
     return LineChartData(
       gridData: FlGridData(
         show: true,
         drawVerticalLine: true,
         horizontalInterval: 1,
-        verticalInterval: 1,
+        verticalInterval: 10,
         getDrawingHorizontalLine: (value) {
           return const FlLine(
             color: Colors.red,
@@ -40,6 +100,7 @@ class _GraphCard extends State<GraphCard> {
           );
         },
       ),
+
       titlesData: FlTitlesData(
         show: true,
         rightTitles: const AxisTitles(
@@ -52,7 +113,13 @@ class _GraphCard extends State<GraphCard> {
           sideTitles: SideTitles(
             showTitles: true,
             reservedSize: 30,
-            interval: 1,
+            interval: 10,
+            getTitlesWidget: (value, meta) {
+              return RotatedBox(
+                quarterTurns: -3,
+                child: Text("${value.round()}"),
+              );
+            }
           ),
         ),
         leftTitles: AxisTitles(
@@ -63,12 +130,13 @@ class _GraphCard extends State<GraphCard> {
           ),
         ),
       ),
+
       borderData: FlBorderData(
         show: true,
         border: Border.all(color: const Color(0xff37434d)),
       ),
       minX: 0,
-      maxX: 11,
+      maxX: (2024 - 1940),
       minY: 0,
       maxY: 6,
       lineBarsData: [
@@ -97,6 +165,7 @@ class _GraphCard extends State<GraphCard> {
   }
 
   TextStyle title = const TextStyle(fontSize: 16, height: 1.75);
+  TextStyle graphLabel = const TextStyle(fontSize: 12);
 
   @override
   Widget build(BuildContext context) {
@@ -113,7 +182,7 @@ class _GraphCard extends State<GraphCard> {
                 child: Padding(
                   padding: const EdgeInsets.fromLTRB(0, 8, 12, 8),
                   child: LineChart(
-                      mainData()
+                    buildData()
                   ),
                 ),
               ),
@@ -122,5 +191,80 @@ class _GraphCard extends State<GraphCard> {
         )
       )
     );
+  }
+}
+
+class _GraphUtilities {
+  static (int, int) getRangeSize(DateTime start, DateTime end) {
+    if(start.isAfter(end)) return (0, 0);
+
+    int days = _getDaysInRange(start, end);
+    if(days < 20) return (days, 0);
+
+    int months = _getMonthsInRange(start, end);
+    if(months < 20) return (months, 1);
+
+    return (_getYearsInRange(start, end), 2);
+  }
+
+  static String getHorizontalLabel(DateTime start, DateTime end, int type, double offset) {
+    if(type == 0) { // day range
+      if(end.month == start.month) return "${start.day + offset.round()}";
+      else {
+        DateTime showedDate = start.add(Duration(days: offset.round()));
+        DateFormat formatter = DateFormat.Md();
+        if(start.year != end.year) formatter = DateFormat.yMd();
+        return formatter.format(showedDate);
+      }
+    }
+    if(type == 1) { // month range
+      DateTime showedDate = start.add(Duration(days: (29.8 * offset).round()));
+      DateFormat formatter = DateFormat.Md();
+      if(start.year != end.year) formatter = DateFormat.yMd();
+      return formatter.format(showedDate);
+    }
+    if(type == 2) {
+      DateFormat formatter = DateFormat.yM();
+      DateTime showedDate = start.add(Duration(days: (365 * offset).round()));
+      return formatter.format(showedDate);
+    }
+    return "";
+  }
+
+  static int getXGap(DateTime start, DateTime end) {
+    int range = getRangeSize(start, end).$1;
+
+    if(range < 24) return 1;
+    if(range < 180) return 10;
+    return 100;
+  }
+
+  static int _getDaysInRange(DateTime start, DateTime end) {
+    Duration duration = end.difference(start);
+    return duration.inDays;
+  }
+
+  static int _getMonthsInRange(DateTime start, DateTime end) {
+    int startYear = start.year;
+    int endYear = end.year;
+    int startMonth = start.month;
+    int endMonth = end.month;
+
+    int totalMonths = 0;
+
+    for(int year = startYear; year <= endYear; year++) {
+      for(int i = (year == startYear) ? startMonth : 1; i <= ((year == endYear) ? endMonth : 12); i++) {
+        totalMonths++;
+      }
+    }
+
+    return totalMonths;
+  }
+
+  static int _getYearsInRange(DateTime start, DateTime end) {
+    int startYear = start.year;
+    int endYear = end.year;
+
+    return endYear - startYear;
   }
 }
--- lib/pages/historical_weather.dart
+++ lib/pages/historical_weather.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:weather_app/components/graph_card.dart';
 
+import '../apis/geo.dart';
+import '../preferences_storage.dart';
+
 class HistoricalWeather extends StatefulWidget {
   const HistoricalWeather({super.key});
 
@@ -10,20 +13,176 @@ class HistoricalWeather extends StatefulWidget {
 }
 
 class _HistoricalWeather extends State<HistoricalWeather> {
-  final TextEditingController _searchTextController = TextEditingController();
-  final TextEditingController _startDateController = TextEditingController();
-  final TextEditingController _endDateController = TextEditingController();
-
-  late DateTime _start;
-  DateTime _end = DateTime.now();
-
-  DateFormat formatter = DateFormat.yMd();
-
   @override
   void initState() {
     super.initState();
     _start = DateTime.fromMillisecondsSinceEpoch(_end.millisecondsSinceEpoch - (65536 * 128));
     updateText();
+    loadFromStorage();
+  }
+
+
+  //region attributes
+  final TextEditingController _searchTextController = TextEditingController();
+  final TextEditingController _startDateController = TextEditingController();
+  final TextEditingController _endDateController = TextEditingController();
+
+  List<Widget> suggestions = [];
+  BuildContext? sheetContext;
+  bool _isGettingLocation = false;
+  bool _isGettingSuggestions = false;
+  DateTime lastWeatherUpdate = DateTime.fromMillisecondsSinceEpoch(0);
+
+  late DateTime _start;
+  DateTime _end = DateTime.now();
+
+  Geo? _selectedGeo;
+
+  DateFormat formatter = DateFormat.yMd();
+
+  bool isWeatherReady = false;
+  //endregion
+
+
+  Future loadFromStorage() async {
+    print("Loading!");
+
+    await PreferencesStorage.initialize();
+
+    if(await PreferencesStorage.readBoolean(SettingPreferences.USE_GPS_DEFAULT) == true) {
+      return _geocodeCurrentLocation();
+    }
+
+    //load last geo (if exists)
+    int? lastLoadTimeFromStorage = await PreferencesStorage.readInteger(PreferencesStorage.GEO_LAST_LOAD);
+    if(lastLoadTimeFromStorage == null) return;
+    String? city = await PreferencesStorage.readString(PreferencesStorage.GEO_CITY);
+    String? fullName = await PreferencesStorage.readString(PreferencesStorage.GEO_FULLNAME);
+    double? lat = await PreferencesStorage.readDouble(PreferencesStorage.GEO_LAT);
+    double? lon = await PreferencesStorage.readDouble(PreferencesStorage.GEO_LON);
+
+    setState(() {
+      _selectedGeo = Geo(lat ?? 0.0, lon ?? 0.0, city: city, fullName: fullName);
+      lastWeatherUpdate = DateTime.fromMillisecondsSinceEpoch(lastLoadTimeFromStorage);
+    });
+
+    print("Loaded!");
+
+    _searchTextController.text = city ?? "";
+    _getWeatherForSelectedGeo();
+  }
+
+  void _openSheet() {
+    if(_searchTextController.text.isNotEmpty) {
+      showModalBottomSheet(
+          context: context,
+          showDragHandle: true,
+          enableDrag: true,
+          builder: (context) {
+            sheetContext = context;
+            return SizedBox(
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text("Suggestions"),
+                                IconButton(
+                                    onPressed: () {
+                                      sheetContext = null;
+                                      Navigator.pop(context);
+                                    },
+                                    icon: const Icon(Icons.close_rounded)
+                                )
+                              ],
+                            )
+                        ),
+                        Expanded( child: SingleChildScrollView(
+                            clipBehavior: Clip.hardEdge,
+                            scrollDirection: Axis.vertical,
+                            child: Expanded(
+                                child: Column(
+                                  children: suggestions,
+                                )))),
+                      ]
+                  ),
+                )
+            );
+          }
+      );
+    }
+  }
+
+  Future _getSuggestions(String searchKey) async {
+    setState(() { _isGettingSuggestions = true; });
+    List<ListTile> liveSuggestions = [];
+
+    await Geo.geocodeLocation(searchKey).then((geos) {
+      geos?.forEach((geo) {
+        print(geo.toString());
+        liveSuggestions.add(ListTile(
+          title: Text(geo.city ?? "UNDEFINED"),
+          subtitle: Text(geo.fullName ?? ""),
+          onTap: () {
+            Geo self = geo;
+            setState(() {
+              _selectedGeo = self;
+              if(geo.city != null) _searchTextController.text = geo.city!;
+              isWeatherReady = false;
+            });
+            if(sheetContext != null) Navigator.pop(sheetContext!);
+            _getWeatherForSelectedGeo();
+          },
+        ));
+      });
+
+      setState(() {
+        _isGettingSuggestions = false;
+        suggestions = liveSuggestions;
+      });
+    });
+  }
+
+  Future _geocodeCurrentLocation() async {
+    setState(() {
+      _isGettingLocation = true;
+      isWeatherReady = false;
+    });
+    try {
+      Geo current = await Geo.getLocation();
+      current = await Geo.geocodeCurrentLocation(current);
+
+      setState(() {
+        if(current.city != null) _searchTextController.text = current.city!;
+        _isGettingLocation = false;
+        _selectedGeo = current;
+      });
+
+    } catch(exception) {
+      print(exception.toString());
+    }
+
+    _getWeatherForSelectedGeo();
+  }
+
+  Future _getWeatherForSelectedGeo() async {
+    if(_selectedGeo == null) return;
+
+
+
+    lastWeatherUpdate = DateTime.now();
+
+    // storeGeo();
+
+    setState(() {
+      isWeatherReady = true;
+    });
   }
 
   void updateText() {
@@ -50,6 +209,7 @@ class _HistoricalWeather extends State<HistoricalWeather> {
       }
 
       updateText();
+      setState(() {});
     }
   }
 
@@ -79,10 +239,10 @@ class _HistoricalWeather extends State<HistoricalWeather> {
                         SearchBar(
                           controller: _searchTextController,
                           onSubmitted: (text) {
-                            //_getSuggestions(text).then((value) => _openSheet());
+                            _getSuggestions(text).then((value) => _openSheet());
                           },
                           trailing: [ IconButton(
-                            icon: (true)?//!_isGettingSuggestions) ?
+                            icon: (!_isGettingSuggestions) ?
                             const Icon(Icons.search_rounded) :
                             const SizedBox(
                                 height: 24,
@@ -90,11 +250,11 @@ class _HistoricalWeather extends State<HistoricalWeather> {
                                 child: CircularProgressIndicator()
                             ),
                             onPressed: () {
-                              String text = "";//_searchTextController.text;
-                              //_getSuggestions(text).then((value) => _openSheet());
+                              String text = _searchTextController.text;
+                              _getSuggestions(text).then((value) => _openSheet());
                             },
                           ) ],
-                          leading: ( false )?//_isGettingLocation) ?
+                          leading: (_isGettingLocation) ?
                           const Padding(
                               padding: EdgeInsets.symmetric(horizontal: 12),
                               child: SizedBox(
@@ -104,7 +264,7 @@ class _HistoricalWeather extends State<HistoricalWeather> {
                               )
                           ) : IconButton(
                             icon: const Icon(Icons.location_on_rounded),
-                            onPressed: () =>  print("lorem"),//_geocodeCurrentLocation(),
+                            onPressed: () => _geocodeCurrentLocation(),
                           ),
                         ),
                         Expanded(
@@ -158,7 +318,7 @@ class _HistoricalWeather extends State<HistoricalWeather> {
                 delegate: SliverChildBuilderDelegate(
                   (BuildContext context, int index) {
                     return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      padding: const EdgeInsets.fromLTRB(12, 0, 12, 12),
                       child: GraphCard(
                         graphStart: _start,
                         graphEnd: _end,
