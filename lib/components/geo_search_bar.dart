fb27140e62a044c38b61445f4779c1e9af6a7976
c4lopsitta <simone@atomtech.cc>
2024-04-23 09:59:36 +0200

    Refactoring searchbar out of current_weather

---
+++ lib/components/geo_search_bar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class GeoSearchBar extends StatefulWidget {
+  GeoSearchBar({
+    super.key
+  });
+
+  @override
+  State<StatefulWidget> createState() => _GeoSearchBar();
+}
+
+class _GeoSearchBar extends State<GeoSearchBar> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SearchBar(
+
+    );
+  }
+}
