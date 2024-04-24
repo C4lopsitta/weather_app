d9d6cf56775fad782c7a375801a856ef2844b3d7
c4lopsitta <simone@atomtech.cc>
2024-04-24 11:14:49 +0200

    Removed dead code

--- lib/preferences_storage.dart
+++ lib/preferences_storage.dart
@@ -3,7 +3,6 @@
 // call time will be added through the Local Preferences Storage API.
 
 import 'dart:io';
-import 'package:flutter/cupertino.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
